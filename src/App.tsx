15f6c8571dc5528d71cea54f5f62ef66c5328d4c
Victory MBENZE <victory.mbenze@proxym-it.com>
2023-11-16 17:55:02 +0100

    test

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from './components/Navbar/Navbar'
+import Test from './components/test/test'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Test />
+    </>
+  )
+}
+
+export default App
